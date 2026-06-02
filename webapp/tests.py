27e7da0327477751c12b7501e4f30262595999f0
aruhinimaycome <aruhini143998@gmail.com>
2026-06-02 13:43:27 +0530

    Update test assertion for root endpoint response

--- webapp/tests.py
+++ webapp/tests.py
@@ -8,7 +8,7 @@ class AppTestCase(unittest.TestCase):
    def test_root_text(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        assert 'this is arun' in response.data
+        assert 'Hello Worlds' in response.data
 
 if __name__ == '__main__':
     unittest.main()
